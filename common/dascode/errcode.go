280f80fab2aa9fde297039197137da3abfbea0bf
lgh <913337456@qq.com>
2021-03-01 17:10:03 +0800

    adjust err code

--- common/dascode/errcode.go
+++ common/dascode/errcode.go
@@ -13,13 +13,13 @@ type DAS_CODE int
 const DAS_SUCCESS DAS_CODE = 0
 
 const (
-	Err_CallIndexer            = 20000
-	Err_Internal               = 20001
-	Err_AccountExpired         = 20002
-	Err_AccountFrozen          = 20003
-	Err_AccountAlreadyRegister = 20004
-	Err_AccountRecordsInvalid  = 20005
-	Err_AccountFormatInvalid   = 20006
-	Err_PubkeyHexFormatInvalid = 20007
-	Err_BaseParamInvalid       = 20008
+	Err_CallIndexer            DAS_CODE = 20000
+	Err_Internal               DAS_CODE = 20001
+	Err_AccountExpired         DAS_CODE = 20002
+	Err_AccountFrozen          DAS_CODE = 20003
+	Err_AccountAlreadyRegister DAS_CODE = 20004
+	Err_AccountRecordsInvalid  DAS_CODE = 20005
+	Err_AccountFormatInvalid   DAS_CODE = 20006
+	Err_PubkeyHexFormatInvalid DAS_CODE = 20007
+	Err_BaseParamInvalid       DAS_CODE = 20008
 )
