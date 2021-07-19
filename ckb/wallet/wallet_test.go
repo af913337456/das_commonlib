b1c727844d69958e711415c6a6967d32a95aa03a
lgh <913337456@qq.com>
2021-07-19 21:16:52 +0800

    fix create ckb wallet bug

--- ckb/wallet/wallet_test.go
+++ ckb/wallet/wallet_test.go
@@ -14,13 +14,12 @@ import (
  */
 
 func Test_RecoverAddrFromLockScriptArgs(t *testing.T) {
-	fmt.Println(GetShortAddressFromLockScriptArgs("0x59ddf0d5d61386b6b3de4b4e8a74c0045f0a410a",false))
-	fmt.Println(GetShortAddressFromLockScriptArgs("0x59ddf0d5d61386b6b3de4b4e8a74c0045f0a410a",true))
+	fmt.Println(GetShortAddressFromLockScriptArgs("",false))
+	fmt.Println(GetShortAddressFromLockScriptArgs("",true))
 }
 
 func Test_CreateWallet(t *testing.T) {
 	j,_ := CreateCKBWallet(false)
-	// {"PriKeyHex":"bd9af72d4d2243ddfec1e05c8409a1ff78f1f13975d49f70ea5259b55d763d11","PubKeyHex":"02c22cb57e628f1639277b79702f9f434878e8e20c88d10e14e846a97f28bb7014","AddressHex":"ckb1qyqg2n9cnmmujkmvrp6xx04dcx7nlw705ezst9lupq"}
 	t.Log(j.Json())
 }
 
