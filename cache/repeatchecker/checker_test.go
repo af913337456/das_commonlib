daa93ebb425c297def4b699ddcd4d963e100446b
lgh <913337456@qq.com>
2021-07-23 01:08:59 +0800

    change cache size

--- cache/repeatchecker/checker_test.go
+++ cache/repeatchecker/checker_test.go
@@ -1,8 +1,15 @@
 package repeatchecker
 
 import (
+	"context"
+	"encoding/hex"
 	"fmt"
+	"github.com/DeAccountSystems/das_commonlib/ckb/celltype"
+	"github.com/DeAccountSystems/das_commonlib/common"
+	"github.com/nervosnetwork/ckb-sdk-go/indexer"
+	"github.com/nervosnetwork/ckb-sdk-go/rpc"
 	"github.com/nervosnetwork/ckb-sdk-go/types"
+	"github.com/nervosnetwork/ckb-sdk-go/utils"
 	"testing"
 )
 
@@ -32,7 +39,36 @@ func Test_NormalCellRepeater(t *testing.T) {
 	fmt.Println(repeater.canUse(pointBytes2))
 }
 
-
+func Test_LoadNormalLiveCell(t *testing.T) {
+	// 20e23005afba0652e6ddf0649f984f78bc3980eb1b83ef104d84ef137b487cf8
+	rpcClient, err := rpc.DialWithIndexerContext(
+		context.TODO(),
+		"http://47.244.126.25:8114", // 47.242.53.82:8114
+		"http://47.244.126.25:8116")
+	if err != nil {
+		panic(fmt.Errorf("init rpcClient failed: %s", err.Error()))
+	}
+	systemScript, err := utils.NewSystemScripts(rpcClient)
+	if err != nil {
+		panic(err)
+	}
+	bys, _ := hex.DecodeString("d6465f6faf694011c770bb4e5aeed5b32865183b")
+	lockScript := types.Script{
+		CodeHash: systemScript.SecpSingleSigCell.CellHash,
+		HashType: "type",
+		Args:     bys,
+	}
+	searchNormalInputKey := &indexer.SearchKey{Script: &lockScript, ScriptType: indexer.ScriptTypeLock}
+	// checker := repeatchecker.NewNormalCellRepeater(120)
+	liveCells, totalCap, err := common.LoadLiveCellsWithSize(rpcClient, searchNormalInputKey, 142*celltype.OneCkb,1,true,true,nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(len(liveCells), totalCap)
+	//for index, item := range liveCells {
+	//	fmt.Println(item.OutPoint.TxHash.String(), index)
+	//}
+}
 
 
 
