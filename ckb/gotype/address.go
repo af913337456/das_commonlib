9d075a616dd856ba89974e61c0315243a361f1eb
lgh <913337456@qq.com>
2021-07-23 15:47:07 +0800

    add PubkeyHashToAddress method

--- ckb/gotype/address.go
+++ ckb/gotype/address.go
@@ -128,3 +128,17 @@ func (r Address) BTCLockScript() (*types.Script, error) {
 		Args:     nil,
 	}, nil
 }
+
+
+func PubkeyHashToAddress(chainType celltype.ChainType, pubKeyHex string) Address {
+	switch chainType {
+	case celltype.ChainType_ETH:
+		return Address(pubKeyHex)
+	case celltype.ChainType_TRON:
+		if strings.HasPrefix(pubKeyHex, tron_chain.TronAddrHexPrefix) {
+			addr, _ := tron_chain.PubkeyHexToBase58(pubKeyHex)
+			return Address(addr)
+		}
+	}
+	return Address(pubKeyHex)
+}
