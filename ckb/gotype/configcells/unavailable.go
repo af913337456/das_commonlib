f4804220dfe8b2366620fcce24b7c2b87fe224e2
lgh <913337456@qq.com>
2021-09-03 11:48:41 +0800

    add CfgUnavailable

--- ckb/gotype/configcells/unavailable.go
+++ ckb/gotype/configcells/unavailable.go
@@ -1,7 +1,6 @@
 package configcells
 
 import (
-	"fmt"
 	"github.com/DeAccountSystems/das_commonlib/ckb/celltype"
 )
 
@@ -15,38 +14,27 @@ import (
 
 type CfgUnavailable struct {
 	Data *ConfigCellChildDataObj
-	MocluData *celltype.ConfigCellAccount
 }
 
 func (c *CfgUnavailable) Ready() bool{
-	return c.Data != nil && c.MocluData != nil && c.MocluData.FieldCount() > 0
+	return c.Data != nil
 }
 
 func (c *CfgUnavailable) Name() string {
-	return "configCellAccount:"
+	return "configCellUnavailable:"
 }
 
 func (c *CfgUnavailable) NotifyData(Data *ConfigCellChildDataObj) error {
 	c.Data = Data
-	if len(c.Data.MoleculeData) == 0 {
-		temp := celltype.ConfigCellAccountDefault()
-		c.MocluData = &temp
-		return nil
-	}
-	obj, err := celltype.ConfigCellAccountFromSlice(c.Data.MoleculeData, false)
-	if err != nil {
-		return fmt.Errorf("ConfigCellAccountFromSlice %s",err.Error())
-	}
-	c.MocluData = obj
 	return nil
 }
 
 func (c *CfgUnavailable) MocluObj() interface{} {
-	return c.MocluData
+	return nil
 }
 
 func (c *CfgUnavailable) Tag() celltype.TableType {
-	return celltype.TableType_ConfigCell_Account
+	return celltype.TableType_ConfigCell_Unavailable
 }
 
 func (c *CfgUnavailable) Witness() *celltype.CellDepWithWitness {
