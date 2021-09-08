848519465a7c76fa8ffd55c5381b982ae784458c
scorpiotzh <835598264@qq.com>
2021-09-08 17:01:04 +0800

    edit 712

--- ckb/gotype/lockscript.go
+++ ckb/gotype/lockscript.go
@@ -22,11 +22,12 @@ import (
  */
 
 type PayTypeLockScriptsRet struct {
-	FeeCellScript    *ckbTypes.Script
-	UserScript       *ckbTypes.Script
-	ScriptType       celltype.LockScriptType
-	DasLockArgsParam celltype.DasLockArgsPairParam
-	Err              error
+	FeeCellScript       *ckbTypes.Script
+	UserScript          *ckbTypes.Script
+	ScriptType          celltype.LockScriptType
+	DasLockArgsParam    celltype.DasLockArgsPairParam
+	DasLockArgsParam712 celltype.DasLockArgsPairParam
+	Err                 error
 }
 
 // ckb argsHexStr, eth address
@@ -84,6 +85,7 @@ func PayTypeLockScripts(sysWallet *ckbTypes.Script, sysScripts *utils.SystemScri
 		userScript            *ckbTypes.Script
 		scriptType            celltype.LockScriptType
 		dasLockParam          celltype.DasLockArgsPairParam
+		dasLockParam712       celltype.DasLockArgsPairParam
 		err                   error
 	)
 	if payType == celltype.ChainType_CKB {
@@ -97,29 +99,39 @@ func PayTypeLockScripts(sysWallet *ckbTypes.Script, sysScripts *utils.SystemScri
 		indexType := scriptType.ToDasLockCodeHashIndexType()
 		userScript, err = address.DasLockScript(indexType)
 		dasLockParam = celltype.DasLockArgsPairParam{HashIndexType: scriptType.ToDasLockCodeHashIndexType(), Script: *userScript}
+		dasLockParam712 = dasLockParam
 		break
 	case celltype.ChainType_ETH:
 		scriptType = celltype.ScriptType_ETH
 		indexType := scriptType.ToDasLockCodeHashIndexType()
 		userScript, err = address.DasLockScript(indexType)
 		dasLockParam = celltype.DasLockArgsPairParam{HashIndexType: indexType, Script: *userScript}
+
+		indexType712 := scriptType.ToDasLockCodeHashIndexType712()
+		userScript712, _ := address.DasLockScript(indexType712)
+		dasLockParam712 = celltype.DasLockArgsPairParam{
+			HashIndexType: indexType712,
+			Script:        *userScript712,
+		}
 		break
 	case celltype.ChainType_TRON:
 		scriptType = celltype.ScriptType_TRON
 		indexType := scriptType.ToDasLockCodeHashIndexType()
 		userScript, err = address.DasLockScript(indexType)
 		dasLockParam = celltype.DasLockArgsPairParam{HashIndexType: indexType, Script: *userScript}
+		dasLockParam712 = dasLockParam
 		break
 	case celltype.ChainType_BTC:
 		scriptType = celltype.ScriptType_BTC
 		indexType := scriptType.ToDasLockCodeHashIndexType()
 		userScript, err = address.DasLockScript(indexType)
 		dasLockParam = celltype.DasLockArgsPairParam{HashIndexType: indexType, Script: *userScript}
+		dasLockParam712 = dasLockParam
 		break
 	default:
-		return PayTypeLockScriptsRet{nil, nil, -1, dasLockParam, errors.New("invalid payType")}
+		return PayTypeLockScriptsRet{nil, nil, -1, dasLockParam, dasLockParam712, errors.New("invalid payType")}
 	}
-	return PayTypeLockScriptsRet{feeCellProviderScript, userScript, scriptType, dasLockParam, err}
+	return PayTypeLockScriptsRet{feeCellProviderScript, userScript, scriptType, dasLockParam, dasLockParam712, err}
 }
 
 func GetScriptTypeFromLockScript(ckbSysScript *utils.SystemScripts, lockScript *ckbTypes.Script) (celltype.LockScriptType, error) {
